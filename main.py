f5d48dbad54ab95c02f16aaff3d05968a19b0fcb
Ater <aterlycoris+work@gmail.com>
2026-05-11 16:43:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import os
+import discord
+import asyncio  # 追加
+from discord.ext import commands
+from dotenv import load_dotenv
+from database import BotDatabase
 
+# 設定の読み込み
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+class main(bot):
+  def __init__(self):
+    
